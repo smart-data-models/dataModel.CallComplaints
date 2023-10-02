e3cb6ed1c30dc652115aeb307c12db651590a5af
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:55:12 +0200

    test of SQL export

--- ComplaintsCollection/schema.sql
+++ ComplaintsCollection/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ComplaintsCollection of the subject dataModel.CallComplaints for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ComplaintsCollection of the subject dataModel.CallComplaints for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ComplaintsCollection_type AS ENUM ('ComplaintsCollection');
-CREATE TABLE ComplaintsCollection (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, groupedAt timestamp, hasCategories json, hasComplaints json, id text, location json, name text, owner json, seeAlso json, source text, type ComplaintsCollection_type);
+CREATE TABLE ComplaintsCollection (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, groupedAt TIMESTAMP, hasCategories JSON, hasComplaints JSON, name TEXT, owner JSON, source TEXT, type ComplaintsCollection_type);
