ade80d12b7ffa06b08ce344fcd8a7e837ae04cbe
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:55:07 +0200

    test of SQL export

--- CallUser/schema.sql
+++ CallUser/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model CallUser of the subject dataModel.CallComplaints for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model CallUser of the subject dataModel.CallComplaints for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE CallUser_type AS ENUM ('CallUser');
-CREATE TABLE CallUser (address json, aliasName text, alternateName text, areaServed text, contactPhone text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, makesComplaints json, name text, owner json, seeAlso json, source text, surname text, type CallUser_type);
+CREATE TABLE CallUser (address JSON, aliasName TEXT, alternateName TEXT, areaServed TEXT, contactPhone TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, makesComplaints JSON, name TEXT, owner JSON, source TEXT, surname TEXT, type CallUser_type);
