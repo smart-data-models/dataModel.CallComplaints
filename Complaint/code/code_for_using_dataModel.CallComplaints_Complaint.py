96c23b9fc7c9790c68ad6976690c4f5ea40296cd
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:31:01 +0200

    created example of code

--- Complaint/code/code_for_using_dataModel.CallComplaints_Complaint.py
+++ Complaint/code/code_for_using_dataModel.CallComplaints_Complaint.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Complaint"
 subject = "dataModel.CallComplaints"
-category = "{'type': 'Property', 'value': 'Low pressure'}"
+category = "Low pressure"
 attribute = "category"
 value = category
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isFiledTo = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Organization:01'}"
+isFiledTo = "urn:ngsi-ld:Organization:01"
 attribute = "isFiledTo"
 value = isFiledTo
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isMadeBy = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:User:01'}"
+isMadeBy = "urn:ngsi-ld:User:01"
 attribute = "isMadeBy"
 value = isMadeBy
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isPartOf = {'type': 'Relationship', 'object': ['urn:ngsi-ld:ComplaintsCollection:01', 'urn:ngsi-ld:ComplaintsCollection:02']}
+isPartOf = ['urn:ngsi-ld:ComplaintsCollection:01', 'urn:ngsi-ld:ComplaintsCollection:02']
 attribute = "isPartOf"
 value = isPartOf
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
