0b14f33e310b756bd2a8c74e3497077672d2917d
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:31:08 +0200

    created example of code

--- ComplaintsOrganization/code/code_for_using_dataModel.CallComplaints_ComplaintsOrganization.py
+++ ComplaintsOrganization/code/code_for_using_dataModel.CallComplaints_ComplaintsOrganization.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ComplaintsOrganization"
 subject = "dataModel.CallComplaints"
-contactPoint = {'type': 'Property', 'value': [{'name': 'ContactPoint', 'telephone': '25830000', 'email': 'contact@wbl.com.cy', 'contactType': '24 hours Customer Service', 'url': 'https://www.wbl.com.cy/en/page/home'}]}
+contactPoint = [{'name': 'ContactPoint', 'telephone': '25830000', 'email': 'contact@wbl.com.cy', 'contactType': '24 hours Customer Service', 'url': 'https://www.wbl.com.cy/en/page/home'}]
 attribute = "contactPoint"
 value = contactPoint
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-receivesComplaints = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Complaint:01', 'urn:ngsi-ld:Complaint:02']}
+receivesComplaints = ['urn:ngsi-ld:Complaint:01', 'urn:ngsi-ld:Complaint:02']
 attribute = "receivesComplaints"
 value = receivesComplaints
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
